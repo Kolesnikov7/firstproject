74e481df5c88ef5c57b4f4c86f7aefb35b22f2aa
petrpetrov <1111>
2020-03-31 19:53:17 +0300

    homework1 - changed added string

--- src/main/java/firstpackage/Firstclass.java
+++ src/main/java/firstpackage/Firstclass.java
@@ -5,6 +5,7 @@ public class Firstclass {
     public static void main(String[] args) {
     int cheslo = 12;
     String b = "hello";
+    String a = "test";
         System.out.printf(cheslo+b);
     }
 
