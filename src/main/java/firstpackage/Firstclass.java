b3a8dd99e99377e649fe0f3eb5b0dd7da2a6a7fa
petrpetrov <1111>
2020-03-31 19:26:19 +0300

    master - added firstclass

---
+++ src/main/java/firstpackage/Firstclass.java
@@ -0,0 +1,14 @@
+package firstpackage;
+
+public class Firstclass {
+
+    public static void main(String[] args) {
+    int cheslo = 12;
+    String b = "hello";
+        System.out.printf(cheslo+b);
+    }
+
+    public static void test (){
+
+    }
+}
