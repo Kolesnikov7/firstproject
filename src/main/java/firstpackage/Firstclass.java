414ada4c3051c8ffe36e46e446847047439da50f
petrpetrov <1111>
2020-04-23 20:55:18 +0300

    Testbranch - test project branch

--- src/main/java/firstpackage/Firstclass.java
+++ src/main/java/firstpackage/Firstclass.java
@@ -5,9 +5,10 @@ public class Firstclass {
     public static void main(String[] args) {
     int cheslo = 12;
     String b = "hello";
-    String a = "test";
+    String a = "test1";
         System.out.printf(cheslo+b);
     }
+
 
     public static void test (){
 
